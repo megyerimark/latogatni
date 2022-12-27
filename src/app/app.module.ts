4f1491cbf06bdd02a9cfe22847dafe073568cbb9
Márk Megyeri <megyko1234@gmail.com>
2022-12-27 17:38:40 +0100

    Git Komponens létrehozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,14 @@
+
+ /*
+* File: app.module
+* Author: Megyeri Márk Máté
+* Copyright: 2022, Megyeri Márk Máté
+* Group: Szoft II/N
+* Date: 2022-12-27
+* Github: https://github.com/megyerimark
+*/
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,12 +16,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { StorageComponent } from './storage/storage.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    StorageComponent
+    StorageComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
