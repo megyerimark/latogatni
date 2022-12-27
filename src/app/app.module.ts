9f5a9b959232b5398f2d20a9df4497a19e4efd26
Márk Megyeri <megyko1234@gmail.com>
2022-12-27 17:34:34 +0100

    Tároló Komponens létrehozva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { StorageComponent } from './storage/storage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    StorageComponent
   ],
   imports: [
     BrowserModule,
