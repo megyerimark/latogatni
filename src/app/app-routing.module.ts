9189fe4d2095cdeea97448f89eec60793e4f9b7d
Márk Megyeri <megyko1234@gmail.com>
2022-12-27 17:44:20 +0100

    Storage Komponens kész

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+ /*
+* File: app.app-routing
+* Author: Megyeri Márk Máté
+* Copyright: 2022, Megyeri Márk Máté
+* Group: Szoft II/N
+* Date: 2022-12-27
+* Github: https://github.com/megyerimark
+*/
+
+
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GitComponent } from './git/git.component';
+import { StorageComponent } from './storage/storage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "storage", component:StorageComponent},
+  {path: "git", component:GitComponent},
+  {path: "about", component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/storage/storage.component.ts
+++ src/app/storage/storage.component.ts
@@ -1,3 +1,14 @@
+ /*
+* File: storage.component
+* Author: Megyeri Márk Máté
+* Copyright: 2022, Megyeri Márk Máté
+* Group: Szoft II/N
+* Date: 2022-12-27
+* Github: https://github.com/megyerimark
+*/
+
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +17,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./storage.component.scss']
 })
 export class StorageComponent implements OnInit {
+  
+
+  data:any = []
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.data = [
+      {name: "Székesfehérvár", county:"Fejér", area:170.89,population:  95545 },
+      {name: "Szombathely", county: "Vas", area: 97.5, population:	78324},
+      {name: "Sopron", county: "Győr-Moson-Sopron", area: 169.04, population: 62900},
+      {name: "Zalaegerszeg", county: "Zala", area: 102.42, population: 55470},
+      {name: "Sárvár", county: "Vas", area:	64.65, population: 15072},
+      {name: "Lenti", county: "Zala", area:	73.79, population: 7348},
+      {name: "Zalakaros", county: "Zala", area: 17.17, population: 1936}
+
+    ]
   }
 
 }
